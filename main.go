c5944db7868c570eb2ed43f2daedcd61279311cd
Jake Walsh <jake.donigan.walsh@monsanto.com>
2019-08-20 18:02:07 -0500

    added basic file reading logic -jw

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("failed to open file with err %s", err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
